eb74f120e8c516912dda62e81d58294ef544152e
HyeonGyu Park <gusrb1502@gmail.com>
2023-06-15 15:12:34 +0900

    Http 데이터를 객체로 처리하는 방법

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -32,12 +32,51 @@ public class RequestController {
     // [Request sample]
     // POST http://localhost:8080/hello/request/form/param
     // Header
-    // Content type: application/x-www-form-urlencoded
+    //  Content type: application/x-www-form-urlencoded
     // Body
-    // name=Robbie&age=95
+    //  name=Robbie&age=95
     @PostMapping("/form/param")
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+
+    //@ModelAttribute : Body 부분에 들어온 Query String 방식의 데이터를 객체에 맵핑에서 가져올수 있다.
+    // 생략이 가능하다. 하지만 Spring은 해당 파라미터(매개변수)가 SimpleValueType이라면 @RequestParam으로 간주하고 아니라면
+    // @ModelAttribute가 생략되어있다 판단합니다.
+    // SimpleValueType은 원시타입(int), Wrapper타입(Integer), Date등의 타입을 의미합니다.
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+
 }
